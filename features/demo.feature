d13556f7bfd7efb490b8c86bcf6defb320f8bb39
Marcis <marcis.krumins@testdevlab.com>
2017-05-04 11:58:03 +0300

    login & signup first cases

--- features/demo.feature
+++ features/demo.feature
@@ -3,3 +3,15 @@ Feature: Demo Feature
     Scenario: Open SignUp Page
         Given I am on Appimation home page
         When I click Try Now
+        Then I enter blahblah@blah.meh in SignUp email
+        Then I enter Password123 in SignUp passwords
+        Then I enter somethingSomethingInTheMonthOfMay in SignUp project name
+        Then I cancel SignUp
+
+    Scenario: Log in to the page
+        Given I am on Appimation home page
+        When I open login modal
+        Then I enter blah@blah.meh in Login email
+        And I enter 11111111 in Login password
+        And I select Login button
+        And I have successfully logged in
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,23 +1,48 @@
 When(/^I am on Appimation home page/) do
   visit('/')
-  
+  find(:id, 'start_button').visible?
+  find(:id, 'login-b').visible?
+  find(:id, 'signup-b').visible? 
 end
 
 Then(/^I click Try Now/) do
-  
-end
-Then(/^I enter (.*) in SignUp email/) do
-  
+  find(:id, 'start_button').click
 end
 
-Then(/^I enter (.*) in SignUp passwords/) do
-  
+Then(/^I enter (.*) in SignUp email/) do |email|
+  find(:xpath, '//*[@id="signup"]/descendant::input[@type="email"]').send_keys email
 end
 
-Then(/^I enter (.*) in SignUp project name/) do
-  
+Then(/^I enter (.*) in SignUp passwords/) do |password|
+  find(:xpath, '//*[@id="signup"]/descendant::input[@name="password1"]').send_keys password
+  find(:xpath, '//*[@id="signup"]/descendant::input[@name="password2"]').send_keys password
+end
+
+Then(/^I enter (.*) in SignUp project name/) do |pName|
+  find(:xpath, '//*[@id="signup"]/descendant::input[@name="project_name"]').send_keys pName
 end
 
 Then(/^I cancel SignUp/) do
-  
+  find(:xpath, '//*[@id="signup"]/form/descendant::img[@class="closecross"]').click
+end
+
+When(/^I open login modal/) do
+  find(:xpath, '//*[@id="login-b"]').click
+end
+
+Then(/^I enter (.*) in Login email/) do |email|
+  find(:xpath, '//*[@id="login"]/form/div/descendant::input[@type="email"]').send_keys email
+end
+
+Then(/^I enter (.*) in Login password/) do |password|
+  find(:xpath, '//*[@id="login"]/form/descendant::input[@type="password"]').send_keys password
+end
+
+Then(/^I select Login button/) do
+  find(:xpath, '//*[@id="login"]/form/button').click
+end
+
+Then(/^I have successfully logged in/) do
+  find(:xpath, '//*[@id="testDataTitle"]').visible?
+  find(:xpath, '//*[@id="logoutButton"]').visible?
 end
